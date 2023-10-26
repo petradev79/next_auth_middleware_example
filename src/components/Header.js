4ea9ffbd268592cb9ea8afa2b2cf45a0d213bb31
Ivan Petrovic <petraivan1010@gmail.com>
2023-10-26 22:13:00 +0200

    add navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 'use client';
 
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 const Header = () => {
+  const { data: session } = useSession();
+  const role = session?.user?._doc.role;
   return (
     <div className='header'>
       <h2>Logo</h2>
+      <div className='navigation'>
+        <Link href='/'>Home</Link>
+        <Link href='/about'>About</Link>
+        {role && (role === 'admin' || role === 'manager') && (
+          <Link href='/contact'>Contact</Link>
+        )}
+        {role && role === 'admin' && <Link href='/dashboard'>Dashboard</Link>}
+      </div>
       <button onClick={() => signOut()}>Sign out</button>
     </div>
   );
